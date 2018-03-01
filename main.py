3b2593d7e1f61a0d48fa4c03f510857145a2b050
Stefano Martinallo <stefano@sherlock.bike>
2018-03-01 22:18:38 +0100

    Choose the least busy car for ride
    Signed-off-by: Stefano Martinallo <stefano@sherlock.bike>

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+class Vehicle(object):
+    def __init__(self, id):
+        self.id = id
+        self.time = 0
+        self.position = (0,0)
+        self.trips = []
+
+
 def waiting_time(vehicle_time, earliest_start):
     return max(0, earliest_start-vehicle_time)
 
@@ -9,40 +17,35 @@ def travel_time(p0, p1):
     return abs( (p0[0]-p1[0]) + ((p0[1]-p1[1])) )
 
 
-def print_sol(vehicle_trips):
-    for trips in vehicle_trips:
-        print("{} {}".format(len(trips), " ".join(map(str, trips))))
+def print_sol(vehicles):
+    for vehicle in sorted(vehicles, key=lambda x: x.id):
+        print("{} {}".format(len(vehicle.trips), " ".join(map(str, vehicle.trips))))
+
     return
 
 
 def solve(R, C, F, N, B, T):
-    vehicle_idx = 0
-    vehicle_time = 0
-    vehicle_position = (0,0)
-    vehicle_trips = [[] for _ in range(F)]
+    vehicles = [Vehicle(idx) for idx in range(F)]
 
     for ride_idx in range(N):
-        if vehicle_idx >= F: break
+        vehicles.sort(key=lambda x: x.time)
 
         r_start, c_start, r_goal, c_goal, earliest_start, latest_finish = map(int, raw_input().split(' '))
         p_start = (r_start, c_start)
         p_goal = (r_goal, c_goal)
 
-        time = waiting_time(vehicle_time, earliest_start)
-        time += travel_time(vehicle_position, p_start)
+        time = waiting_time(vehicles[0].time, earliest_start)
+        time += travel_time(vehicles[0].position, p_start)
         time += travel_time(p_start, p_goal)
 
-        if vehicle_time + time > T:
-            vehicle_idx += 1
-            vehicle_time = 0
-            vehicle_position = (0,0)
+        if vehicles[0].time + time > T:
+            continue
 
-        if vehicle_idx < F:
-            vehicle_time += time
-            vehicle_position = p_goal
-            vehicle_trips[vehicle_idx].append(ride_idx)
+        vehicles[0].time += time
+        vehicles[0].position = p_goal
+        vehicles[0].trips.append(ride_idx)
 
-    print_sol(vehicle_trips)
+    print_sol(vehicles)
     return
 
 
